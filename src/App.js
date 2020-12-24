453d6753db74678853fd0d4d9cb9f4f79d79ccd8
Allison Machado Gonçalves <allison.goncalves.ext@siemens.com>
2020-12-24 19:04:05 +0000

    Improve footer and header layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
         <header>
           {/* Fixed navbar */}
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a className="navbar-brand" href="/">Fixed navbar</a>
+            <a className="navbar-brand" href="/">Node Boost</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
@@ -17,31 +17,32 @@ export default class App extends React.Component {
                 <li className="nav-item active">
                   <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">Link</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="/" tabIndex={-1} aria-disabled="true">Disabled</a>
-                </li>
               </ul>
-              <form className="form-inline mt-2 mt-md-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
             </div>
           </nav>
         </header>
         {/* Begin page content */}
         <main role="main" className="flex-shrink-0">
           <div className="container">
-            <h1 className="mt-5">Sticky footer with fixed navbar</h1>
-            <p className="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>main &gt; .container</code>.</p>
-            <p>Back to <a href="/docs/4.5/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+            <span>Content</span> 
           </div>
         </main>
-        <footer className="footer mt-auto py-3">
+        <footer className="footer mt-auto py-3 bg-light">
           <div className="container">
-            <span className="text-muted">Place sticky footer content here.</span>
+            <div className="row my-4">
+              <div className="col-sm">
+              <a className="text-secondary font-weight-bold" href="/">GitHub</a>
+              </div>
+              <div className="col-sm">
+              <a className="text-secondary font-weight-bold" href="/">About</a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm">
+                Designed and built with love by <a href="https://github.com/allisonmachado">Allison Machado</a>.
+                Currently v0.1.0. Code licensed <a href="/">MIT</a>.
+              </div>
+            </div>
           </div>
         </footer>
       </React.Fragment>
