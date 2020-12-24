fdf4a803fe702753aa443375dce04320f86907cd
Allison Machado Gonçalves <allison.goncalves.ext@siemens.com>
2020-12-24 19:14:08 +0000

    Set margin and padding of main content layout

--- src/App.js
+++ src/App.js
@@ -23,8 +23,38 @@ export default class App extends React.Component {
         </header>
         {/* Begin page content */}
         <main role="main" className="flex-shrink-0">
-          <div className="container">
-            <span>Content</span> 
+          <div className="container py-5 my-5">
+            <h1>Main Title</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit lectus et risus pellentesque, at maximus diam sollicitudin. Quisque elementum, orci id molestie aliquam, massa odio pulvinar eros, nec imperdiet augue augue et massa. Integer tempor luctus posuere. Sed non justo odio. Quisque ornare porttitor vulputate. Suspendisse metus sem, pulvinar blandit dictum sit amet, sodales non diam. Maecenas diam tortor, pulvinar tincidunt molestie ac, semper eget nisl. Vestibulum eleifend mollis risus, id ultrices diam viverra ac. Ut sit amet ex urna.
+            </p>
           </div>
         </main>
         <footer className="footer mt-auto py-3 bg-light">
