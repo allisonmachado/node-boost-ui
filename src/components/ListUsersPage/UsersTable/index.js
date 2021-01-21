bf30abe51bf0fbf05412174c1f10ddd07245daf1
Allison Machado Gonçalves <aallison93@gmail.com>
2021-01-21 23:55:35 +0000

    Create remove user front-end interaction

--- src/components/ListUsersPage/UsersTable/index.js
+++ src/components/ListUsersPage/UsersTable/index.js
@@ -4,8 +4,10 @@ export default class UsersTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      userSelected: null,
     }
+    this.deleteButtonClick = this.deleteButtonClick.bind(this);
   }
 
   async componentDidMount() {
@@ -13,8 +15,19 @@ export default class UsersTable extends React.Component {
     this.setState({ users });
   }
 
+  deleteButtonClick(user) {
+    this.setState(() => ({
+      userSelected: user,
+    }));
+  }
+
+  deleteUser(user) {
+    this.setState({ users: [] });
+  }
+
   render() {
     return (
+      <React.Fragment>
         <table className="table table-hover table-responsive-sm">
           <thead className="thead-light">
             <tr>
@@ -30,13 +43,33 @@ export default class UsersTable extends React.Component {
               <td>{user.surname}</td>
               <td>{user.email}</td>
               <td>
-                <button type="button" className="btn btn-danger">
+                <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#confirmationModal" onClick={() => this.deleteButtonClick(user)}>
                   <i className="bi-trash"></i>
                 </button>
               </td>
             </tr>)}
           </tbody>
         </table>
+        <div className="modal fade" id="confirmationModal" tabIndex={-1} aria-labelledby="confirmationModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="confirmationModalLabel">Confirm</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                {this.state.userSelected ? <span>Delete user "{this.state.userSelected.name + this.state.userSelected.surname}"?</span> : <span></span> }
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">No</button>
+                <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => this.deleteUser(this.state.userSelected)}>Yes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
